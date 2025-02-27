6a37a57767a5aea53aa983b62c6999bd91d6f85d
Roman Gardukevich <roman@gardukevich.com>
2025-02-27 23:55:01 +0200

    Migrate to color assets

--- KinoPub/Shared/Extensions/UIColor+KPAdditions.swift
+++ KinoPub/Shared/Extensions/UIColor+KPAdditions.swift
@@ -8,71 +8,71 @@ extension UIColor {
     }
 
     public class var kpLightGreen: UIColor {
-        return UIColor(red: 0.380, green: 0.753, blue: 0.490, alpha: 1.00)
+        UIColor(resource: .kpLightGreen)
     }
 
     @nonobjc class var kpWhite: UIColor {
-        return UIColor(white: 216.0 / 255.0, alpha: 1.0)
+        UIColor(resource: .kpWhite)
     }
 
     @nonobjc class var kpWhiteTwo: UIColor {
-        return UIColor(white: 230.0 / 255.0, alpha: 1.0)
+        UIColor(resource: .kpWhiteTwo)
     }
 
     @nonobjc class var kpTangerine: UIColor {
-        return UIColor(red: 255.0 / 255.0, green: 152.0 / 255.0, blue: 0.0, alpha: 1.0)
+        UIColor(resource: .kpTangerine)
     }
 
     @nonobjc class var kpBlack: UIColor {
-        return UIColor(white: 30.0 / 255.0, alpha: 1.0)
+        UIColor(resource: .kpBlack)
     }
 
     @nonobjc class var kpGreyishBrownTwo: UIColor {
-        return UIColor(white: 86.0 / 255.0, alpha: 1.0)
+        UIColor(resource: .kpGreyishBrownTwo)
     }
 
     @nonobjc class var kpMarigold: UIColor {
-        return UIColor(red: 255.0 / 255.0, green: 201.0 / 255.0, blue: 0.0, alpha: 1.0)
+        UIColor(resource: .kpMarigold)
     }
 
     @nonobjc class var kpGreyish: UIColor {
-        return UIColor(white: 184.0 / 255.0, alpha: 1.0)
+        UIColor(resource: .kpGreyish)
     }
 
     @nonobjc class var kpOffWhite: UIColor {
-        return UIColor(red: 255.0 / 255.0, green: 244.0 / 255.0, blue: 228.0 / 255.0, alpha: 1.0)
+        UIColor(resource: .kpOffWhite)
     }
 
     @nonobjc class var kpLemonGreen: UIColor {
-        return UIColor(red: 182.0 / 255.0, green: 255.0 / 255.0, blue: 0.0, alpha: 1.0)
+        UIColor(resource: .kpLemonGreen)
     }
 
     @nonobjc class var kpGreyishTwo: UIColor {
-        return UIColor(red: 172.0 / 255.0, green: 164.0 / 255.0, blue: 152.0 / 255.0, alpha: 1.0)
+        UIColor(resource: .kpGreyishTwo)
     }
 
     @nonobjc class var kpGreyishBrown: UIColor {
-        return UIColor(red: 68.0 / 255.0, green: 65.0 / 255.0, blue: 60.0 / 255.0, alpha: 1.0)
+        UIColor(resource: .kpGreyishBrown)
     }
 
     @nonobjc class var kpWhite10: UIColor {
-        return UIColor(white: 255.0 / 255.0, alpha: 0.1)
+        UIColor(resource: .kpWhite10)
     }
 
     @nonobjc class var kpBlackTwo: UIColor {
-        return UIColor(red: 43.0 / 255.0, green: 43.0 / 255.0, blue: 43.0 / 255.0, alpha: 1.0)
+        UIColor(resource: .kpBlackTwo)
     }
 
     @nonobjc class var kpBlack30: UIColor {
-        return UIColor(white: 0.0, alpha: 0.3)
+        UIColor(resource: .kpBlack30)
     }
 
     @nonobjc class var kpOffWhiteSeparator: UIColor {
-        return UIColor(red: 255.0 / 255.0, green: 244.0 / 255.0, blue: 228.0 / 255.0, alpha: 0.16)
+        UIColor(resource: .kpOffWhiteSeparator)
     }
 
     @nonobjc class var kpBlackForTable: UIColor {
-        return UIColor(white: 17.0 / 255.0, alpha: 1.0)
+        UIColor(resource: .kpBlackForTable)
     }
 
     func isLight() -> Bool {
